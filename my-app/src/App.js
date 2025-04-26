18c45656806370942173bcabd82819fb8f1d0ef9
meameng <53843842+meameng@users.noreply.github.com>
2025-04-26 06:08:54 +0000

    tablet works

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
+import {useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const clickBtn = () => {
+    setValue((prev) => prev +1);
+  } 
+  console.log ("I start all the time ");
+  const eftEx = () => {
+    console.log ("I view only one time ");
+  }
+  useEffect("eftEx",[]);
+
+  // 6.2부터~~
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{counter}</h1>
+      <Button onClick={clickBtn} text={"Click Me!"}/>
     </div>
   );
 }
